7e98a0562dfa274650bd21c7589bc9c60282e7b7
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-19 17:54:28 -0700

    Intial files structure created using scraper activity as a base model to create this app

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 1992;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/week18Populater");
+
+
+app.get("/scrape", function (req, res) {
+
+    axios.get("http://www.theverge.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each(function (i, element) {
+
+            var result = {};
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    return res.json(err);
+                });
+        });
+
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
