3c330a26b7dda32736f04f9e700e12a396096510
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-25 09:58:21 -0700

    mistakes corrected

--- controllers/notes-controller.js
+++ controllers/notes-controller.js
@@ -10,54 +10,55 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/vergeScraper";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI)
 
-router.get("/getnotes/:id", function(req,res){
-    db.Article.findOne({id:req.params.id}).populate("note").then(function(resNotes){
+router.get("/getnotes/:id", function (req, res) {
+    db.Article.findOne({ id: req.params.id }).populate("note").then(function (resNotes) {
         console.log("this are the notes:", resNotes)
         res.json(resNotes)
-    }).catch(function(err){
+    }).catch(function (err) {
         res.json(err);
     })
 });
 
-router.get("/getonenote/:id", function(req,res){
-    db.Note.findOne({id:req.params.id}).then(function(resNote){
+router.get("/getonenote/:id", function (req, res) {
+    db.Note.findOne({ id: req.params.id }).then(function (resNote) {
         console.log("this are the notes:", resNote)
         res.json(resNote)
-    }).catch(function(err){
+    }).catch(function (err) {
         res.json(err);
     })
 });
 
-router.get("/makenote", function(req,res){
-    req.body = {title, body, articleId}
+router.get("/makenote", function (req, res) {
+    req.body = { title, body, articleId }
     var note = {
-        title, 
+        title,
         body
-    }l
-    db.Note.create(note).then(function(result){
-        db.Article.findOneAndUpdate(_id: articleId, {$push:{notes:result._id}},{new:true})
-        .then(function(data){
-            res.json(result);
-        })
-        .catch(function(err){
+    }
+    db.Note.create(note).then(function (result) {
+        db.Article.findOneAndUpdate({ _id: articleId }, { $push: { notes: result._id } }, { new: true })
+            .then(function (data) {
+                res.json(result);
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
+    })
+        .catch(function (err) {
             res.json(err);
         })
     })
-    .catch(function(err){
-        res.json(err);
-})
 
-router.post('/deleteNote', function(req,res){
-    req.body = {articleId, noteId}
-    db.Note
-        .remove({_id: noteId})
-        .then(function(result){
-            res.json(result)
-        })
-        .catch(function(err){
-            res.json(err);
-        })
-})
+    router.post('/deleteNote', function (req, res) {
+        req.body = { articleId, noteId }
+        db.Note
+            .remove({ _id: noteId })
+            .then(function (result) {
+                res.json(result)
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
+    })
 
 
-module.exports = router;
+    module.exports = router;
